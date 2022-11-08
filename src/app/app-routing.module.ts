4bc56036275720a6d55c49de90c67aa12daa1b4b
fabio.polato@git.csteam.com <fabio.polato@csteam.com>
2022-11-08 18:05:26 +0100

    Componenti principali e inizio programmazione del Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevicesComponent } from './components/devices/devices.component';
+import { LightsComponent } from './components/lights/lights.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { ThermostatsComponent } from './components/thermostats/thermostats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'lights', component: LightsComponent },
+  { path: 'thermostats', component: ThermostatsComponent },
+  { path: 'devices', component: DevicesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { LightsComponent } from './components/lights/lights.component';
+import { ThermostatsComponent } from './components/thermostats/thermostats.component';
+import { DevicesComponent } from './components/devices/devices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    LightsComponent,
+    ThermostatsComponent,
+    DevicesComponent
   ],
   imports: [
     BrowserModule,
