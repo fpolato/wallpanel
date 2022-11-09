b56942290cfd352abe4b10f6481f93be7497c3ac
fabio.polato <fabio.polato@csteam.com>
2022-11-09 16:45:04 +0100

    Orario screen saver

--- src/app/components/screensaver/screensaver.component.ts
+++ src/app/components/screensaver/screensaver.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'screensaver',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScreensaverComponent implements OnInit {
 
-  constructor() { }
+  today: Date = new Date();
+  timeSeparatorVisible: boolean = true;
+  
+  timeSeparatorSub:Subscription;
+  dateTimeSub:Subscription;
+
+  constructor() {
+
+    const timeSeparatorSource = interval(500);
+    const dateTimeSource = interval(1000);
+
+    this.timeSeparatorSub = timeSeparatorSource.subscribe(val => {
+      this.timeSeparatorVisible = !this.timeSeparatorVisible;
+    });
+    this.dateTimeSub = dateTimeSource.subscribe(val => {
+      this.today = new Date();
+    });
+  }
 
   ngOnInit(): void {
   }
