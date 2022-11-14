a52774ff65562b4817ff536abea553f0ef9554a6
fabio.polato <fabio.polato@csteam.com>
2022-11-14 18:10:26 +0100

    Oggetto luce

--- src/app/components/widgets/light/light.component.ts
+++ src/app/components/widgets/light/light.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'wp-light',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./light.component.scss']
 })
 export class LightComponent implements OnInit {
+
+  @Input() lightName: string = '';
+  @Input() lightOn: boolean = false;
 
   constructor() { }
 
